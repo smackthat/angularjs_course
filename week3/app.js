c4e93396785304ebcb27f2c544ec4333cf264510
Johannes Stenberg <johannes.stenberg@luukku.com>
2017-01-12 10:46:07 +0200

    Minor modifications

--- week3/app.js
+++ week3/app.js
@@ -2,7 +2,6 @@
 //This was a fun & challenging task! :-)
 
 //The standard code for reference, the minified is in use
-/*
 (function() {
 	'use strict';
 	
@@ -64,8 +63,9 @@
 					 if (word.length === 0)
 						 return foundItems;
 					 var items = result.data.menu_items;
+					 console.log(items);
 					 for (var i=0; i < items.length; i++) {
-						if (items[i].name.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+						if (items[i].description.toLowerCase().indexOf(word.toLowerCase()) !== -1 || items[i].name.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
 							foundItems.push(items[i]);
 						}
 					 }
@@ -109,7 +109,7 @@
 	}
 	
 })();
-*/
+
 
 //Here's the minified code
-!function(){"use strict";function n(n){var t=this;t.query="",t.found=[],t.nothingFound=!1,t.logItems=function(e){var o=n.getMatchedMenuItems(e);o.then(function(n){t.found=n,0===t.found.length?t.nothingFound=!0:t.nothingFound=!1})["catch"](function(n){console.log(n)})},t.removeItem=function(n){t.found.splice(n,1)}}function t(n,t){var e=this;e.getMatchedMenuItems=function(e){var o=[],r=n({method:"GET",url:t});return r.then(function(n){if(0===e.length)return o;for(var t=n.data.menu_items,r=0;r<t.length;r++)-1!==t[r].name.toLowerCase().indexOf(e.toLowerCase())&&o.push(t[r]);return o})}}function e(){var n={templateUrl:"loader/itemsloaderindicator.template.html",scope:{onRemove:"&",items:"<",nothing:"<"},controller:o,controllerAs:"list",bindToController:!0};return n}function o(){var n=this;n.isEmpty=function(){return n.nothing===!0?!0:!1}}angular.module("NarrowItDownApp",[]).controller("NarrowItDownController",n).service("MenuSearchService",t).constant("ApiPath","https://davids-restaurant.herokuapp.com/menu_items.json").directive("foundItems",e),n.$inject=["MenuSearchService"],t.$inject=["$http","ApiPath"]}();
+////!function(){"use strict";function n(n){var t=this;t.query="",t.found=[],t.nothingFound=!1,t.logItems=function(e){var o=n.getMatchedMenuItems(e);o.then(function(n){t.found=n,0===t.found.length?t.nothingFound=!0:t.nothingFound=!1})["catch"](function(n){console.log(n)})},t.removeItem=function(n){t.found.splice(n,1)}}function t(n,t){var e=this;e.getMatchedMenuItems=function(e){var o=[],r=n({method:"GET",url:t});return r.then(function(n){if(0===e.length)return o;for(var t=n.data.menu_items,r=0;r<t.length;r++)-1!==t[r].name.toLowerCase().indexOf(e.toLowerCase())&&o.push(t[r]);return o})}}function e(){var n={templateUrl:"loader/itemsloaderindicator.template.html",scope:{onRemove:"&",items:"<",nothing:"<"},controller:o,controllerAs:"list",bindToController:!0};return n}function o(){var n=this;n.isEmpty=function(){return n.nothing===!0?!0:!1}}angular.module("NarrowItDownApp",[]).controller("NarrowItDownController",n).service("MenuSearchService",t).constant("ApiPath","https://davids-restaurant.herokuapp.com/menu_items.json").directive("foundItems",e),n.$inject=["MenuSearchService"],t.$inject=["$http","ApiPath"]}();
